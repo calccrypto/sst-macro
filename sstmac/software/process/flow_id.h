1346ea4808be0dbc26879f79337b0ee33252c693
Jason Lee <calccrypto@gmail.com>
2016-07-14 17:49:04 -0400

    Added flow_id type for packet metadata

---
+++ sstmac/software/process/flow_id.h
@@ -0,0 +1,25 @@
+/*
+ *  This file is part of SST/macroscale:
+ *               The macroscale architecture simulator from the SST suite.
+ *  Copyright (c) 2009 Sandia Corporation.
+ *  This software is distributed under the BSD License.
+ *  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+ *  the U.S. Government retains certain rights in this software.
+ *  For more information, see the LICENSE file in the top
+ *  SST/macroscale directory.
+ */
+
+#ifndef SSTMAC_SOFTWARE_PROCESS_FLOWID_H_INCLUDED
+#define SSTMAC_SOFTWARE_PROCESS_FLOWID_H_INCLUDED
+
+#include <sprockit/spkt_config.h>
+
+namespace sstmac {
+namespace sw {
+
+typedef int flow_id;
+
+}
+} // end of namespace sstmac
+
+#endif
